a3c29d0caac023c21999c01861edd0da0b260513
Jonathan Magnet <j.magnet@outlook.com>
2023-06-06 14:55:31 +0200

    index version 1

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,16 @@
 class MatchesController < ApplicationController
   def index
+    @matches = Match.order(date: :desc, level: :asc)
+
+    if params[:query].present?
+      @matches = @matches.where("address ILIKE ?", "%#{params[:query]}%")
+    end
+
+    if params[:date].present?
+      @matches = @matches.where("date ILIKE ?", "%#{params[:date]}%")
+    end
   end
+
 
   def show
   end
