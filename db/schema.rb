b24cb7f2b659b8908bb72599e707d642ef6b7b13
Jonathan Magnet <j.magnet@outlook.com>
2023-06-07 17:47:58 +0200

    modif belong to

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_06_07_102833) do
-
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
