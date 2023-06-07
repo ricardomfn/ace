dda3083ef94389564230d26a1d1d7e32948244cd
mathieu jelsch <mathieu.jelsch@gmail.com>
2023-06-07 12:40:25 +0200

    add archived to matches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_145036) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_102833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_145036) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.boolean "archived"
     t.index ["user_id"], name: "index_matches_on_user_id"
   end
 
