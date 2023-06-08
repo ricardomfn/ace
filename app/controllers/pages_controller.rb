bc68e3c934374dc0fca4b43db95ff496c605cacf
Jonathan Magnet <j.magnet@outlook.com>
2023-06-08 13:01:29 +0200

    modif des leagues

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
   def profile
+    user = current_user.id
+    # membership = Membership.find(params[])
+    # @leagues = League.where(user_id: user)
+    members = Membership.where(user_id: user)
+    @leagues = []
+    members.each do |member|
+      @leagues << member.league
+    end
   end
-
-  
 end
