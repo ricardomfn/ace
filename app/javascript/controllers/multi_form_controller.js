4ed239993cff95161805bfea36adc2a6b4dc7a54
Jonathan Magnet <j.magnet@outlook.com>
2023-06-19 14:42:57 +0200

    form ok

--- app/javascript/controllers/multi_form_controller.js
+++ app/javascript/controllers/multi_form_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="multi-form"
 export default class extends Controller {
 
-  static targets = ["link","first","ligne","wrap","image","nom"]
+  static targets = ["link","first","ligne","wrap","image","nom","naming","suivant"]
 
 
   hideLogo() {
@@ -11,13 +11,23 @@ export default class extends Controller {
     this.firstTarget.style.color = "green";
     this.wrapTarget.classList.add("wrapper-green");
     this.ligneTarget.style.color = "green";
+    this.nomTarget.classList.remove("hide");
+    this.namingTarget.classList.remove("hide");
     this.imageTargets.forEach((image) => {
       image.style.display = "none";
     });
+    this.suivantTarget.classList.add("d-none");
   }
 
-  // nom(){
-  //   this.nomTarget.classList.remove("d-none");
+
+  handleImage(){
+    this.suivantTarget.removeAttribute('disabled');
+  }
+
+  // disable() {
+  //   this.element.setAttribute("disabled", "");
   // }
+
+
 
 }
